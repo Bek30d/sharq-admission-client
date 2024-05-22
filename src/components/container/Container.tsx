dd2a320899e19c247d8cf156292a64edc8e5ae42
BekFr <shamsiyevbekzod7666@gmail.com>
2024-05-22 17:56:59 +0500

    feat: main page done without navbar and shadcn added

---
+++ src/components/container/Container.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+const Container = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="max-w-[1560px] mx-auto xl:px-20 lg:px-6 md:px-10 sm:px-2 px-4">
+      {children}
+    </div>
+  );
+};
+
+export default Container;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="py-3.5 h-12">Navbar</div>;
+};
+
+export default Navbar;
