3f83dc68f8774783c3c468e73c2eee03f6440553
Bek30d <shamsiyevbekzod2000@gmail.com>
2024-05-24 01:27:13 +0500

    fix: hotfix

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -9,14 +9,18 @@ import HamburgerButton from "../hamburger-button/HamburgerButton";
 import { useIndexStore } from "@/store";
 import Logo from '../../../public/assets/logo.svg'
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const Navbar = () => {
   const { isOpenBurger } = useIndexStore()
   const path = usePathname()
 
   return <div className="h-12 bg-[#18324D] xl:-mx-20 lg:-mx-6 md:-mx-10 sm:-mx-2 -mx-4 xl:px-20 lg:px-6 md:px-10 sm:px-2 px-4">
-    <div className={`md:block hidden ${isOpenBurger ? '' : ''}`}>
-      <div className="flex items-center justify-end gap-12 pt-3">
+    <div className="md:flex  pt-1.5 justify-between items-center hidden">
+      <Link href="/" className={path === '/' ? 'hidden' : ''}>
+        <Image src={Logo} alt="logo" className="w-24" />
+      </Link>
+      <div className="flex items-center justify-end gap-12">
         <a href="tel:+998712345678" className="text-white font-medium text-sm">+998 77 500-50-60</a>
         <div className="flex gap-4">
           <a href="#">
@@ -40,7 +44,9 @@ const Navbar = () => {
       className={`md:hidden flex flex-col opacity-100 w-full `}
     >
       <div className={`${path === '/' ? 'justify-end' : 'justify-between'} flex items-center h-full`}>
-        <Image src={Logo} alt="logo" className={`${path === '/' ? 'hidden' : ''} w-24`} />
+        <Link href={"/"}>
+          <Image src={Logo} alt="logo" className={`${path === '/' ? 'hidden' : ''} w-24`} />
+        </Link>
         <HamburgerButton />
       </div>
       <div className={`absolute top-0 left-0 z-10 h-screen w-full flex flex-col bg-gradient-to-b from-[#18324D] to-[#3874B3] justify-center items-center gap-16 flex-1 duration-300 ${!isOpenBurger ? "translate-x-[100vw]" : "translate-x-0"}`}>
