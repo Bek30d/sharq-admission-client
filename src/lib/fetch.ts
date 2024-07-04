f8dd4ebca3f856620614e53a2a4eb64a27ae4c26
Islom <80814465+Alixujaev@users.noreply.github.com>
2024-07-04 16:39:16 +0500

    Update fetch.ts
    url changed

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: 'https://sharqapi.onedata.uz/api',
   headers: {
     Accept: 'application/json',
     'Authorization': `Bearer ${localStorage.getItem('access_token')}`,
